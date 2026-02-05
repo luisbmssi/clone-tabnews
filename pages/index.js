167161c352d75f0c11528703481c66a27f2fbde0
luisbmssi <118406920+luisbmssi@users.noreply.github.com>
2026-02-05 19:02:06 +0000

    Adicionado scripts `lint:check e lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,42 +1,39 @@
-import { useState } from "react"
-import Lottie from "lottie-react"
-import animationData from "../public/like.json"
-import styles from "../styles/home.module.css"
+import { useState } from "react";
+import Lottie from "lottie-react";
+import animationData from "../public/like.json";
+import styles from "../styles/home.module.css";
 
 function Home() {
-    const [show, setShow] = useState(false)
+  const [show, setShow] = useState(false);
 
-    return (
-        <div className={styles.wrapper}>
-            {!show
-                ?
-                (
-                    <button className={styles.button} onClick={() => setShow(true)}>Clique aqui</button>
-                )
-                :
-                (
-                    <div className={styles.card}>
-                        <Lottie
-                            animationData={animationData}
-                            loop
-                            style={{ width: 220 }}
-                        />
+  return (
+    <div className={styles.wrapper}>
+      {!show ? (
+        <button className={styles.button} onClick={() => setShow(true)}>
+          Clique aqui
+        </button>
+      ) : (
+        <div className={styles.card}>
+          <Lottie animationData={animationData} loop style={{ width: 220 }} />
 
-                        <p className={styles.letter}>
-                            Oi amorzinho ❤️
-                            <br /><br />
-                            Às vezes a gente não precisa de grandes discursos,
-                            só de pequenos gestos que dizem muito.
-                            <br /><br />
-                            Que essa mensagem te lembre que você é importante,
-                            especial e merece tudo de bom que existe.
-                            <br /><br />
-                            Te amo!
-                        </p>
-                    </div>
-                )}
+          <p className={styles.letter}>
+            Oi amorzinho ❤️
+            <br />
+            <br />
+            Às vezes a gente não precisa de grandes discursos, só de pequenos
+            gestos que dizem muito.
+            <br />
+            <br />
+            Que essa mensagem te lembre que você é importante, especial e merece
+            tudo de bom que existe.
+            <br />
+            <br />
+            Te amo!
+          </p>
         </div>
-    )
+      )}
+    </div>
+  );
 }
 
-export default Home
+export default Home;
