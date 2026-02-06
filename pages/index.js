bdc6a08222134976ef2faec8a95fb4ddf7208228
luisbmssi <118406920+luisbmssi@users.noreply.github.com>
2026-02-06 23:52:12 +0000

    Alterado página principal: Em construção

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,4 @@
 import { useState } from "react";
-import Lottie from "lottie-react";
-import animationData from "../public/like.json";
 import styles from "../styles/home.module.css";
 
 function Home() {
@@ -8,30 +6,7 @@ function Home() {
 
   return (
     <div className={styles.wrapper}>
-      {!show ? (
-        <button className={styles.button} onClick={() => setShow(true)}>
-          Clique aqui
-        </button>
-      ) : (
-        <div className={styles.card}>
-          <Lottie animationData={animationData} loop style={{ width: 220 }} />
-
-          <p className={styles.letter}>
-            Oi amorzinho ❤️
-            <br />
-            <br />
-            Às vezes a gente não precisa de grandes discursos, só de pequenos
-            gestos que dizem muito.
-            <br />
-            <br />
-            Que essa mensagem te lembre que você é importante, especial e merece
-            tudo de bom que existe.
-            <br />
-            <br />
-            Te amo!
-          </p>
-        </div>
-      )}
+      <h1 className={styles.letter}>Em construção</h1>
     </div>
   );
 }
