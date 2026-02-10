9aeb0ca8c1bf52936cd6e530cf7bf1c523696157
luisbmssi <118406920+luisbmssi@users.noreply.github.com>
2026-02-10 02:47:31 +0000

    Finaliza endpoint de `status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,11 +1,31 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1;");
+  const updatedAt = new Date().toISOString();
 
-  console.log(result.rows[0]);
+  const versionQuery = await database.query("SHOW server_version;");
+  const version = versionQuery.rows[0].server_version;
+
+  const maxConnectionsQuery = await database.query("SHOW max_connections;");
+  const maxConnections = maxConnectionsQuery.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+
+  const openedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1",
+    values: [databaseName],
+  });
+  const openedConnections = openedConnectionsResult.rows[0].count;
+
   response.status(200).json({
-    message: "Endpoint funcional",
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        max_connections: parseInt(maxConnections),
+        opened_connections: openedConnections,
+        version: version,
+      },
+    },
   });
 }
 
